f89230c8e16614ebe8c117a99f47e54836a9881f
haduart <haduart@gmail.com>
2015-03-09 23:19:42 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,37 @@
+(defproject phoenix-demo-webapp ""
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+
+                 [ring/ring-core "1.3.0"]
+                 [bidi "1.15.0"]
+                 [hiccup "1.0.5"]
+                 [garden "1.2.1"]
+                 [ring-middleware-format "0.4.0"]
+
+                 [jarohen/phoenix.modules.aleph "0.0.1"]
+                 [jarohen/phoenix.modules.cljs "0.0.1"]
+
+                 [org.clojure/clojurescript "0.0-2760"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [jarohen/flow "0.3.0-alpha3"]
+
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.9"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.1"]
+                 [org.apache.logging.log4j/log4j-core "2.1"]]
+
+  :exclusions [org.clojure/clojure]
+
+  :plugins [[jarohen/phoenix "0.0.7"]
+            [jarohen/simple-brepl "0.2.1"]]
+
+  :phoenix/config "phoenix-demo-webapp-config.edn"
+
+  :aliases {"dev" "phoenix"
+            "build" ["phoenix" "uberjar"]
+            "start" ["trampoline" "phoenix"]})
---
+++ src/phoenix_demo_webapp/service/css.clj
@@ -0,0 +1,6 @@
+(ns phoenix-demo-webapp.service.css
+  (:require [garden.core :as css]))
+
+(defn site-css []
+  (css/css []))
+
